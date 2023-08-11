3e3c8afc6cf46936e8419b4533fd73254df346e5
Arkan Arsalan Amanullah <arkan.arsalan.amanullah@gmail.com>
2023-08-11 20:54:47 +0700

    adding function body for parse_argument function

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import sys
 import pytube
 import argparse
 
-
+# The name of this file could be the same in argparse library
 def parse_argument():
-    ...
+    parser = argparse.ArgumentParser(description="Download youtube video")
+    
+    # Add an argument
+    parser.add_argument("-t", "--type", help="Type of download (video or audio)", type=str, required=True)
+    parser.add_argument("-l", "--link", help="Youtube video link", type=str, required=True)
+    parser.add_argument("-lp", "--location_path", help="Location path", type=str, required=True)
+    parser.add_argument("-r", "--resolution", help="Video resolution (lowest/default/highest)", type=str, required=False, default="highest")
+
+    arguments = parser.parse_args()
+
+    return arguments
 
 
-def validate_link():
+def validate_link(link):
     ...
 
 
@@ -20,8 +30,8 @@ def download_audio():
 
 
 def main():
-    ...
-
+    arguments = parse_argument()
+    print(arguments)
 
 if __name__ == "__main__":
     main()
