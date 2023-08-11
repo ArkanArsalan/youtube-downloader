b4f6f178d6b23491b1f9bb5be809f9196610b0cf
Arkan Arsalan Amanullah <arkan.arsalan.amanullah@gmail.com>
2023-08-11 16:49:53 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import sys
+import pytube
+import argparse
+
+
+def parse_argument():
+    ...
+
+
+def validate_link():
+    ...
+
+
+def download_video():
+    ...
+
+
+def download_audio():
+    ...
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
